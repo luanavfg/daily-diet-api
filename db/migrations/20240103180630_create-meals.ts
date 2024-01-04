2d77aa64b0f06020149068dc0a8a7690e254138f
luanavfg <luanavefago1@gmail.com>
2024-01-04 14:26:38 -0300

    adjust: add itens in migration

--- db/migrations/20240103180630_create-meals.ts
+++ db/migrations/20240103180630_create-meals.ts
@@ -3,8 +3,10 @@ import type { Knex } from 'knex'
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('meals', (table) => {
     table.uuid('id').primary()
-    table.text('title').notNullable()
+    table.text('name').notNullable()
+    table.text('description').notNullable()
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.boolean('is_on_diet').notNullable()
   })
 }
 
