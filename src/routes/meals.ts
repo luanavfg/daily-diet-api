6db71ffc423335ae561dea9a96ad73453cd2414e
luanavfg <luanavefago1@gmail.com>
2024-01-05 16:34:06 -0300

    feat: add routes to get and delete meals

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -4,6 +4,36 @@ import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
 
 export async function mealsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const meals = await knex('meals').select()
+
+    return { meals }
+  })
+
+  app.get('/:id', async (request) => {
+    const getMealBodySchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getMealBodySchema.parse(request.params)
+    const meal = await knex('meals').where('id', id).first()
+
+    return { meal }
+  })
+
+  app.delete('/:id', async (request, reply) => {
+    const getMealBodySchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getMealBodySchema.parse(request.params)
+    await knex('meals').where('id', id).del()
+
+    return reply.status(200).send({
+      message: 'Meal successfully deleted',
+    })
+  })
+
   app.post('/', async (request, reply) => {
     const createMealBodySchema = z.object({
       name: z.string(),
