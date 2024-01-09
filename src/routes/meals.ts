7d5acf86a8f9150d5bd0608ea82893bbb8570072
luanavfg <luanavefago1@gmail.com>
2024-01-09 08:25:04 -0300

    feat: add check session id

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -2,24 +2,37 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function mealsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const meals = await knex('meals').select()
+  app.get(
+    '/',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async () => {
+      const meals = await knex('meals').select()
 
-    return { meals }
-  })
+      return { meals }
+    },
+  )
 
-  app.get('/:id', async (request) => {
-    const getMealBodySchema = z.object({
-      id: z.string().uuid(),
-    })
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const getMealBodySchema = z.object({
+        id: z.string().uuid(),
+      })
 
-    const { id } = getMealBodySchema.parse(request.params)
-    const meal = await knex('meals').where('id', id).first()
+      const { id } = getMealBodySchema.parse(request.params)
+      const meal = await knex('meals').where('id', id).first()
 
-    return { meal }
-  })
+      return { meal }
+    },
+  )
 
   app.delete('/:id', async (request, reply) => {
     const getMealBodySchema = z.object({
@@ -34,24 +47,30 @@ export async function mealsRoutes(app: FastifyInstance) {
     })
   })
 
-  app.post('/', async (request, reply) => {
-    const createMealBodySchema = z.object({
-      name: z.string(),
-      description: z.string(),
-      isOnDiet: z.boolean(),
-    })
+  app.post(
+    '/',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const createMealBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        isOnDiet: z.boolean(),
+      })
 
-    const { name, description, isOnDiet } = createMealBodySchema.parse(
-      request.body,
-    )
+      const { name, description, isOnDiet } = createMealBodySchema.parse(
+        request.body,
+      )
 
-    await knex('meals').insert({
-      id: randomUUID(),
-      name,
-      description,
-      is_on_diet: isOnDiet,
-    })
+      await knex('meals').insert({
+        id: randomUUID(),
+        name,
+        description,
+        is_on_diet: isOnDiet,
+      })
 
-    return reply.status(201).send()
-  })
+      return reply.status(201).send()
+    },
+  )
 }
