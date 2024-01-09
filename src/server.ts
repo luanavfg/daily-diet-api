2b2b4de4ac5bcb293b251a3a700dc770cd2f05ac
luanavfg <luanavefago1@gmail.com>
2024-01-09 08:14:00 -0300

    feat: add users route and cookies

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import fastify from 'fastify'
 import { env } from './env'
 import { mealsRoutes } from './routes/meals'
+import { usersRoutes } from './routes/users'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
+app.register(cookie)
+
 app.register(mealsRoutes, {
   prefix: 'meals',
+})
+
+app.register(usersRoutes, {
+  prefix: 'users',
 })
 
 app
