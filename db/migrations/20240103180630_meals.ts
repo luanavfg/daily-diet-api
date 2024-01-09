fb748d1871254898952944191cbc21bca42775b1
luanavfg <luanavefago1@gmail.com>
2024-01-09 08:13:25 -0300

    adjust: add attributes to meals migration

--- db/migrations/20240103180630_meals.ts
+++ db/migrations/20240103180630_meals.ts
@@ -3,9 +3,11 @@ import type { Knex } from 'knex'
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('meals', (table) => {
     table.uuid('id').primary()
+    table.uuid('user_id').references('users.id').notNullable()
     table.text('name').notNullable()
     table.text('description').notNullable()
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('updated_at')
     table.boolean('is_on_diet').notNullable()
   })
 }
