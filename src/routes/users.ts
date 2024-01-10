3256217d6cb48155a3f551c1a81671062fd586bc
luanavfg <luanavefago1@gmail.com>
2024-01-10 08:21:31 -0300

    adjust: adjust users routes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,6 +4,35 @@ import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
 
 export async function usersRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createUserBodySchema = z.object({
+      name: z.string(),
+      email: z.string(),
+    })
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
+    const { name, email } = createUserBodySchema.parse(request.body)
+
+    await knex('users').insert({
+      id: randomUUID(),
+      name,
+      email,
+      session_id: sessionId,
+    })
+
+    return reply.status(201).send()
+  })
+
   app.get('/', async () => {
     const users = await knex('users').select()
 
@@ -32,34 +61,5 @@ export async function usersRoutes(app: FastifyInstance) {
     return reply.status(200).send({
       message: 'User successfully deleted',
     })
-  })
-
-  app.post('/', async (request, reply) => {
-    const createUserBodySchema = z.object({
-      name: z.string(),
-      email: z.string(),
-    })
-
-    let sessionId = request.cookies.sessionId
-
-    if (!sessionId) {
-      sessionId = randomUUID()
-
-      reply.cookie('sessionId', sessionId, {
-        path: '/',
-        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-      })
-    }
-
-    const { name, email } = createUserBodySchema.parse(request.body)
-
-    await knex('users').insert({
-      id: randomUUID(),
-      name,
-      email,
-      session_id: sessionId,
-    })
-
-    return reply.status(201).send()
   })
 }
